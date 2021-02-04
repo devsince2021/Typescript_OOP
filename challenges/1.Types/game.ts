f161c6bae90c5bce47ae894998b53693d89c7c9d
devsince2021 <dev.ryu2021@gmail.com>
2021-02-04 16:08:03 +0900

    feat: Try challenges

--- challenges/1.Types/game.ts
+++ challenges/1.Types/game.ts
@@ -1,12 +1,48 @@
 /**
  * Let's make a game 🕹
  */
-console.log(position); // { x: 0, y: 0}
-move('up');
-console.log(position); // { x: 0, y: 1}
-move('down');
-console.log(position); // { x: 0, y: 0}
-move('left');
-console.log(position); // { x: -1, y: 0}
-move('right');
-console.log(position); // { x: 0, y: 0}
+{
+  // type alias : type 내가 타입을 정의 할 수 있다.
+  // string literal types
+  // union type: Or - type alias를 사용하는 방법 중 하나.
+  
+  type Direction = 'up' | 'down' | 'left' | 'right';
+
+  // type alias
+  type Position = {
+    x: number;
+    y: number;
+  };
+
+  const position: Position = {
+    x: 0,
+    y: 0,
+  };
+
+  const move = (direction: Direction): void => {
+    switch (direction) {
+      case 'up':
+        position.y++;
+        return;
+      case 'down':
+        position.y--;
+        return;
+      case 'left':
+        position.x--;
+        return;
+      case 'right':
+        position.x++;
+        return;
+    }
+  }
+
+  console.log(position); // { x: 0, y: 0}
+  move('up');
+  console.log(position); // { x: 0, y: 1}
+  move('down');
+  console.log(position); // { x: 0, y: 0}
+  move('left');
+  console.log(position); // { x: -1, y: 0}
+  move('right');
+  console.log(position); // { x: 0, y: 0}
+}
--- challenges/1.Types/loading.ts
+++ challenges/1.Types/loading.ts
@@ -2,10 +2,13 @@
   /**
    * Print Loading State
    */
+{
+  //type alias, define type
   type LoadingState = {
     state: 'loading';
   };
 
+  //type alias, define type
   type SuccessState = {
     state: 'success';
     response: {
@@ -13,14 +16,28 @@
     };
   };
 
+  //type alias, define type
   type FailState = {
     state: 'fail';
     reason: string;
   };
 
+  // type alias
+  // union type
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  const printLoginState = (loadState: ResourceLoadState) => {
+    if (loadState.state === 'loading') {
+      console.log(`👀 ${loadState.state}...`);
+    } else if (loadState.state === 'success') {
+      console.log(`😃 ${loadState.response.body}`);
+    } else {
+      console.log(`😱 ${loadState.reason}`);
+    }
+  }
+
+    printLoginState({ state: 'loading' }); // 👀 loading...
+    printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
+    printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  }
 }
